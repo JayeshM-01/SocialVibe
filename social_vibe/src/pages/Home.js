b29d3647066448b634b173bd66ed005b5f7c9c59
Jayesh <jayashmore278@gmail,com>
2024-09-30 11:42:00 +0530

    Initialized Frontend

---
+++ social_vibe/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Sidebar } from '../components/Sidebar'
+import { PostCard } from '../components/PostCard'
+
+export const Home = () => {
+  return (
+    <div>
+        <Sidebar/>
+        <PostCard/>
+    </div>
+  )
+}
