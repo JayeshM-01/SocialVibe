f7499a245f2091692016a3dfc94f1c01ab638cd9
Jayesh <jayashmore278@gmail,com>
2024-10-06 18:19:52 +0530

    changes

--- backend/cloudinary/index.js
+++ backend/cloudinary/index.js
@@ -3,11 +3,12 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: 'daex3gj8h',
-  api_key: '958145775471671',
-  api_secret: 'xDAo03nnULj6YHAjSEx9aO2HjFw',
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+  
 const storage = new CloudinaryStorage({
   cloudinary,
   folder: 'SocialVibe',
